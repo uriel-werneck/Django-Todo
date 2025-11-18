ffef03a07830f8a895a98b2ef252be90e3db24a6
Uriel Werneck <uriel_werneck@hotmail.com>
2025-11-17 21:51:33 -0300

    feat: add task creation

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import task_list
+from .views import task_list, task_create
 
 urlpatterns = [
     path('', task_list, name='task_list'),
+    path('create/', task_create, name='task_create')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 
 # Create your views here.
 def task_list(request):
     tasks = Task.objects.all()
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+def task_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description', '')
+        Task.objects.create(title=title, description=description)
+        return redirect('task_list')
+    return render(request, 'tasks/task_list.html')
