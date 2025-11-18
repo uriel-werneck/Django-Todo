61da5e8195d2085342e3ba195feeacc5814c0230
Uriel Werneck <uriel_werneck@hotmail.com>
2025-11-17 23:28:56 -0300

    feat: add task update

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import task_list, task_create, task_delete
+from .views import task_list, task_create, task_delete, task_update
 
 urlpatterns = [
     path('', task_list, name='task_list'),
     path('create/', task_create, name='task_create'),
-    path('delete/<int:id>/', task_delete, name='task_delete')
+    path('delete/<int:id>/', task_delete, name='task_delete'),
+    path('update/<int:id>/', task_update, name='task_update')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -15,8 +15,15 @@ def task_create(request):
 
 def task_delete(request, id):
     task = get_object_or_404(Task, id=id)
-
     if request.method == 'POST':
         task.delete()
+        return redirect('task_list')
+    return render(request, 'tasks/task_list.html')
+
+def task_update(request, id):
+    task = get_object_or_404(Task, id=id)
+    if request.method == 'POST':
+        task.completed = 'completed' in request.POST
+        task.save()
         return redirect('task_list')
     return render(request, 'tasks/task_list.html')
